c56094b6275dcd413c55acf349e61fa3b263d9d0
Hritvik Gupta <hritvikgupta@brb-6707.apng.wlan.private.upenn.edu>
2024-07-17 11:18:51 -0400

    Added workflows for commit

--- update_readme.py
+++ update_readme.py
@@ -1,17 +1,43 @@
-import json
+import os
+import requests
 
-# Function to fetch data from GitHub API
-def fetch_org_data():
-    with open('org.json') as f:
-        return json.load(f)
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+REPO_OWNER = 'Verma-Lab'
+REPO_NAME = '.github'
 
-# Fetch organization data
-org_data = fetch_org_data()
+headers = {
+    "Authorization": f"token {GITHUB_TOKEN}",
+    "Accept": "application/vnd.github.v3+json"
+}
+
+def fetch_commit_data():
+    url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/commits"
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+def fetch_contributors():
+    url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/contributors"
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+commit_data = fetch_commit_data()
+contributors = fetch_contributors()
+
+# Generate commit history
+commit_history = ""
+for commit in commit_data[:5]:  # Get the latest 5 commits
+    commit_message = commit['commit']['message']
+    author = commit['commit']['author']['name']
+    date = commit['commit']['author']['date']
+    commit_history += f"- {commit_message} by {author} on {date}\n"
+
+# Generate contributors list
+contributors_list = "\n".join([f"- [{contributor['login']}](https://github.com/{contributor['login']})" for contributor in contributors])
 
 # Prepare README content
 readme_content = f"""
-# Welcome to Verma-Lab
-
 ## Hi there 👋
 
 <!--
@@ -24,6 +50,8 @@ readme_content = f"""
 🍿 Fun facts - what does your team eat for breakfast?
 🧙 Remember, you can do mighty things with the power of [Markdown](https://docs.github.com/github/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax)
 -->
+
+# Welcome to Verma-Lab
 
 ![Screenshot 2024-07-16 at 3 46 20 PM](https://github.com/user-attachments/assets/de609b6b-c700-4d76-9ec9-ccc8763291cd)
 
@@ -44,6 +72,9 @@ A brief description of what Project 1 does.
 ### [Project 2](https://github.com/Verma-Lab/project-2)
 A brief description of what Project 2 does.
 
+## Latest Commits
+{commit_history}
+
 ## Contributors
 - [Contributor 1](https://github.com/contributor1)
 - [Contributor 2](https://github.com/contributor2)
@@ -56,20 +87,22 @@ A brief description of what Project 2 does.
 - **Email**: Anurag.Verma@Pennmedicine.upenn.edu
 - **Twitter**:
 
+#
 
-## Stats
+## Repository Badges : .github (Sample Repo, Public)
+</br>
 <div align="center">
-  <p>
-    <img src="https://github-readme-stats.vercel.app/api/top-langs?username=Verma-Lab&show_icons=true&locale=en&layout=compact&theme=blue-green" alt="Top Languages" />
-  </p>
-  <br/>
-  <img alt="streak stats" height="200px" width="400px" src="https://github-readme-streak-stats.herokuapp.com/?user=Verma-Lab&theme=blue-green">
-  <br/>
-  <p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=Verma-Lab&show_icons=true&theme=blue-green" alt="GitHub Stats" /></p>
+  <img src="https://img.shields.io/github/stars/Verma-Lab/.github?style=social" alt="GitHub stars" />
+  <img src="https://img.shields.io/github/forks/Verma-Lab/.github?style=social" alt="GitHub forks" />
+  <img src="https://img.shields.io/github/issues/Verma-Lab/.github" alt="GitHub issues" />
+  <img src="https://img.shields.io/github/issues-pr/Verma-Lab/.github" alt="GitHub pull requests" />
+  <img src="https://img.shields.io/github/last-commit/Verma-Lab/.github" alt="GitHub last commit" />
+  <img src="https://img.shields.io/github/contributors/Verma-Lab/.github" alt="GitHub contributors" />
 </div>
 
 """
 
 # Write the content to .github/profile/README.md
+os.makedirs('.github/profile', exist_ok=True)
 with open('.github/profile/README.md', 'w') as f:
     f.write(readme_content)
